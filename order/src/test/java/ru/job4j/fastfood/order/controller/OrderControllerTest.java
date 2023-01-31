9431e165730170cc0e421a7937f913ef314bd2cc
diac <nikolai.gladkikh@gmail.com>
2023-01-31 14:16:12 +0300

    В OrderControllerTest#whenPlace заменить new ArrayList<>() на Collections.emptyList()

--- order/src/test/java/ru/job4j/fastfood/order/controller/OrderControllerTest.java
+++ order/src/test/java/ru/job4j/fastfood/order/controller/OrderControllerTest.java
@@ -18,7 +18,6 @@ import ru.job4j.fastfood.order.dto.OrderPlacementDto;
 import ru.job4j.fastfood.order.service.OrderService;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -42,7 +41,7 @@ public class OrderControllerTest {
 
     @Test
     public void whenPlace() throws Exception {
-        OrderPlacementDto dto = new OrderPlacementDto(new ArrayList<>(), new Customer());
+        OrderPlacementDto dto = new OrderPlacementDto(Collections.emptyList(), new Customer());
         String requestBody = objectWriter.writeValueAsString(dto);
         Order order = new Order(1, Collections.emptyList(), 0, 0, LocalDateTime.now(), OrderStatus.CREATED);
         Mockito.when(orderService.place(dto)).thenReturn(order);
