13e87d85eb012eb6af4c7959b8af9c0ae8a4374e
diac <nikolai.gladkikh@gmail.com>
2023-02-07 18:31:13 +0300

    Добавить Javadoc к NotificationRepository

--- notification/src/main/java/ru/job4j/fastfood/notification/repository/NotificationRepository.java
+++ notification/src/main/java/ru/job4j/fastfood/notification/repository/NotificationRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.fastfood.domain.model.Notification;
 
+/**
+ * Репозиторий для хранения объектов Notification
+ */
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 }
